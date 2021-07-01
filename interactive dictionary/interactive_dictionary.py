503b0ce3cec8492f3ec5d9cd0662f92d6f0148f5
SAY-droid427 <sayanimallick0218@gmail.com>
2021-07-01 21:31:58 +0530

    Added interactive dictionary project

---
+++ interactive dictionary/interactive_dictionary.py
@@ -0,0 +1,30 @@
+import json
+from difflib import get_close_matches
+
+data=json.load(open('data.json'))
+
+def translate(word):
+    word=word.lower()
+    if word in data:
+        return data[word]
+    elif len(get_close_matches(word,data.keys()))>0:
+        yn=input("Did you mean %s instead?(Y/N)" % get_close_matches(word,data.keys())[0])
+        yn=yn.lower()
+        if(yn=="y"):
+            return data[get_close_matches(word,data.keys())[0]]
+        elif(yn=="n"):
+            return "The word does not exist. Please double check it."
+        else:
+            return "We donot understand your query."
+    else:
+        return "The word does not exist. Please double check it."
+    
+
+word=input("Enter word: ")
+output=translate(word)
+
+if output==list:
+    for item in output:
+        print(item)
+else:
+    print(output)
